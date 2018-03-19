68519483acc9d05f536152efb32cd0455bb6c5e8
edmobe <e.d.mobe@hotmail.com>
2018-03-19 02:08:06 -0600

    Created the game loop.

--- src/com/edmobe/src/Game.java
+++ src/com/edmobe/src/Game.java
@@ -2,26 +2,38 @@ package com.edmobe.src;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
- * A class that defines the game's general parameters.
+ * A class that defines the game's general parameters. Everything inside the class is looped.
  * @author edmobe
  *
  */
-public class Game extends JPanel{
+public class Game extends JPanel implements ActionListener{
 
+	Timer gamelooptimer;
 	
 	private static final long serialVersionUID = 1L;
 
 	public Game() {
 		setFocusable(true);
+		
+		gamelooptimer = new Timer(10, this);
+		gamelooptimer.start();
 	}
 	
 	public void paint(Graphics g) {
 		super.paint(g);
 		
 		Graphics2D g2d = (Graphics2D) g;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		repaint();
 	}	
 }
